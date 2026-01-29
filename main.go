4aa1a0f1218e8ec98da23e843395fa2983ffebb9
Dzhambulat <dzkhasayev@gmail.com>
2026-01-29 22:19:40 +0300

    test transaction retrieving

--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"github.com/fatih/color"
+	"github.com/joho/godotenv"
+	"log"
 	"mempool-monitor/providers"
+	"mempool-monitor/types"
 )
 
 func main() {
-	poolProvider := providers.GetLocalGethProvider("")
-	err := poolProvider.Subscribe()
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		log.Fatal("Error loading .env file")
 	}
+
+	color.Yellow("Starting mempool monitor...")
+	poolProvider := providers.GetLocalGethProvider("")
+	err = poolProvider.Subscribe()
+	if err != nil {
+		log.Fatalf("Failed to subscribe to provider: %v", err)
+	}
+	color.Green("Subscribed to provider successfully!")
+
 	for tx := range poolProvider.GetObserver() {
-		fmt.Println("New tx:")
-		color.Green("Hash: %s", tx.Hash)
-		color.Green("From: %s", tx.From)
-		color.Green("To: %s", tx.To)
+		go func(tx *types.Transaction) {
+			fmt.Println("New tx:")
+			color.Green("Hash: %s", tx.Hash)
+			color.Green("From: %s", tx.From)
+			color.Green("To: %s", tx.To)
+		}(tx)
+
 	}
 }
